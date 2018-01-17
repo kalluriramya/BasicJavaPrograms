30fa759df93179543086f05a44461c511b7486f6
RamyaKalluri <kalluri.ramya333@gmail.com>
2018-01-17 16:03:26 -0600

    First Commit

---
+++ Ramya'sJavaProject/src/com/ramya/java/Abc.java
@@ -0,0 +1,19 @@
+package com.ramya.java;
+
+public class Abc 
+{
+	int i=5;
+	int j=10;
+	public void display()
+	{
+		System.out.println(+i);
+		System.out.println(+j);
+	}
+
+	public static void main(String[] args) 
+	{
+		Abc obj=new Abc();
+		obj.display();
+	}
+
+}
