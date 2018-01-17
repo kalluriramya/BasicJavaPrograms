2702e3cba6222125231cb9c4c080d139b22252bd
RamyaKalluri <kalluri.ramya333@gmail.com>
2018-01-17 16:10:15 -0600

    Second

---
+++ Ramya'sJavaProject/src/com/ramya/java/Abcd.java
@@ -0,0 +1,18 @@
+package com.ramya.java;
+
+public class Abcd {
+
+	int i=5;
+	int j=10;
+	public void display()
+	{
+		System.out.println(+i);
+		System.out.println(+j);
+	}
+	public static void main(String[] args)
+	{
+		Abcd obj=new Abcd();
+		obj.display();
+	}
+
+}
